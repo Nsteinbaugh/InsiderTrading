0267d3cd8f26a5c70c4b815538e654b3ae34d9f6
unknown <steinbaughn@gmail.com>
2022-01-19 17:03:04 -0600

    initial commit

---
+++ src/test/java/com/insidertrading/stockscreener/StockscreenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insidertrading.stockscreener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockscreenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
